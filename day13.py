5527ef641a397cdd61d6729eddc9783bdf8b76d5
Lucas Alegre <lucasnale@gmail.com>
2020-12-14 01:54:14 -0300

    Changed Day 13 approach

--- day13.py
+++ day13.py
@@ -1,5 +1,3 @@
-from functools import reduce
-
 def read_input(filename="inputs/day13.txt"):
     with open(filename) as f:
         lines = [line.strip() for line in f.readlines()]
@@ -8,7 +6,9 @@ def read_input(filename="inputs/day13.txt"):
     return depart, bus_ids
 
 def bus_first_depart(bus, depart):
-    time = (depart // bus) * bus + bus
+    time = (depart // bus) * bus
+    if time != 0:
+        time += bus
     return time
 
 def part1(depart, bus_ids):
@@ -18,31 +18,15 @@ def part1(depart, bus_ids):
     first_bus = bus_ids[timestamps.index(min_time)]
     return first_bus * (min_time - depart)
 
-def chinese_remainder(n, a):
-    sum = 0
-    prod = reduce(lambda a, b: a * b, n)
-    for n_i, a_i in zip(n, a):
-        p = prod // n_i
-        sum += a_i * mul_inv(p, n_i) * p
-    return sum % prod
-
-def mul_inv(a, b):
-    b0 = b
-    x0, x1 = 0, 1
-    if b == 1:
-        return 1
-    while a > 1:
-        q = a // b
-        a, b = b, a % b
-        x0, x1 = x1 - q * x0, x0
-    if x1 < 0:
-        x1 += b0
-    return x1
-
 def part2(bus_ids):
-    n = [int(x) for x in bus_ids if x.isnumeric()]
-    a = [int(bus)*10 - i for i, bus in enumerate(bus_ids) if bus.isnumeric()]
-    return chinese_remainder(n, a)
+    bus = [int(x) for x in bus_ids if x.isnumeric()]
+    offset = [i for i in range(len(bus_ids)) if bus_ids[i].isnumeric()]
+    t, step = 0, 1
+    for (bus, offset) in zip(bus, offset):
+        while (t + offset) % bus:
+            t += step
+        step *= bus
+    return t
 
 depart, bus_ids = read_input()
 print(part1(depart, bus_ids))
